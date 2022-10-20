99b96157e46593266bac6cad83a1c6418eeb9a6c
FrancyAPinzonR <francykale@gmail.com>
2022-10-19 20:35:33 -0500

    Ejercicio temperatura

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
-console.log("Hello");
 // npm i axios
+const axios =require("axios")
+
+console.log("Hello");
+
+let apikey="8e6330a78aa20b41b54f3fcaa3e9994f";
+let city="Bogota, col";
+
+let requestConfig={
+    url:
+        "https://api.openweathermap.org/data/2.5/weather?q="+
+        city +
+        "&appid=" +
+        apikey,
+};
+console.log(requestConfig);
+
+let objPromesa= axios(requestConfig);
+
+objPromesa
+    .then(function (response) {
+        // response.data -> son los datos que envia la API
+        console.log(
+            "La temperatura en Bogota, Colombia es "+(response.data.main.temp-273.15).toFixed(2).toString()+" Grados celcius"
+        );
+        
+    })
+    .catch(function (err) {
+        console.log(err);
+    })
+
